5b56a8d33c4fddd461572e12b4ee15c46146b879
Amirhosein PooyanMoghadam <103578053+AmirhoseinPM@users.noreply.github.com>
2024-10-20 14:13:33 +0330

    Delete src directory

--- src/main/java/com/example/wallet/wallet/WalletRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.wallet.wallet;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface WalletRepository extends CrudRepository<Wallet, Long> {
-    List<Wallet> findByAccount_NationalId(String nationalId);
-}
