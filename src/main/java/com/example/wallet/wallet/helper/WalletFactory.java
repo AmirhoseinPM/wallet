3e8858bf7ab74ac13234ce8b18dcd1c6f7ac201b
Amirhosein PooyanMoghadam <103578053+AmirhoseinPM@users.noreply.github.com>
2024-10-20 19:34:57 +0330

    Delete src directory

--- src/main/java/com/example/wallet/wallet/helper/WalletFactory.java
+++
@@ -1,7 +0,0 @@
-package com.example.wallet.wallet.helper;
-
-import com.example.wallet.wallet.Wallet;
-
-public interface WalletFactory {
-    Wallet getNewWallet(WalletDto walletDto, String nationalId);
-}
